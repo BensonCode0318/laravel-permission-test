cfc70664769205cd5eb4d669dc76aa96347256d2
Benson Chang <bensonchang@damaiapp.com.tw>
2022-01-06 11:11:19 +0800

    chore: add role seeder

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'name' => '一般使用者',
+            'permissions' => [
+                'read_post' => true,
+            ]
+        ]);
+        Role::create([
+            'name' => '編輯者',
+            'permissions' => [
+                'create_post' => true,
+                'update_post' => true,
+                'delete_post' => true,
+            ]
+        ]);
+    }
+}
